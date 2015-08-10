aa0a88de3dcf71824d53c8c624445e5dabe242d3
thetruthmyg <thetruthmyg94@gmail.com>
2015-08-11 02:03:14 +0800

    界面调整

--- app/src/main/java/com/team1/easyhelp/home/fragment/HomeFragment.java
+++ app/src/main/java/com/team1/easyhelp/home/fragment/HomeFragment.java
@@ -22,10 +22,6 @@ public class HomeFragment extends Fragment {
         return fragment;
     }
 
-//    public HomeFragment() {
-//        // Required empty public constructor
-//    }
-
 //    @Override
 //    public void onCreate(Bundle savedInstanceState) {
 //        super.onCreate(savedInstanceState);
--- app/src/main/java/com/team1/easyhelp/home/fragment/NeighborFragment.java
+++ app/src/main/java/com/team1/easyhelp/home/fragment/NeighborFragment.java
@@ -22,10 +22,6 @@ public class NeighborFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
-//
-//    public NeighborFragment() {
-//        // Required empty public constructor
-//    }
 
 //    @Override
 //    public void onCreate(Bundle savedInstanceState) {
@@ -67,20 +63,6 @@ public class NeighborFragment extends Fragment {
 //        mListener = null;
 //    }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-//    public interface OnFragmentInteractionListener {
-//        // TODO: Update argument type and name
-//        public void onFragmentInteraction(Uri uri);
-//    }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
